a634791026cd2630cfe0bb881e14e9693dfd25eb
cerenunlu <cerennlu01@outlook.com>
2022-02-16 18:26:01 +0300

    Check Auth added

--- app/routes/employeeRoutes.js
+++ app/routes/employeeRoutes.js
@@ -1,18 +1,18 @@
 module.exports=app=>{
     const employee=require("../controllers/employeeController");
-
+    const checkAuth = require('../middleware/checkAuth');
 var router=require("express").Router();
 
 //create new customer
-router.post("/",employee.create);
+router.post("/",checkAuth,employee.create);
 //find all customers
-  router.get("/",employee.findAll);
+ router.get("/",checkAuth,employee.findAll);
  //find customer by id
-router.get("/:id",employee.findOne);
+router.get("/:id",checkAuth,employee.findOne);
 // //update a customer
-router.put("/:id",employee.update);
+router.put("/:id",checkAuth,employee.update);
 // //delete customer
-router.delete("/:id",employee.delete);
+router.delete("/:id",checkAuth,employee.delete);
 
 app.use("/api/employee",router);
 
