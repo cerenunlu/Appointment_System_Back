b7a79b5066dc557aadb56cd0f4cf67e974768e80
cerenunlu <cerennlu01@outlook.com>
2022-02-16 18:26:22 +0300

    Check Auth added

--- app/routes/roleRoutes.js
+++ app/routes/roleRoutes.js
@@ -1,18 +1,18 @@
 module.exports = app => {
   const role = require("../controllers/roleController");
-
+  const checkAuth = require('../middleware/checkAuth');
   var router = require("express").Router();
 
   //create new customer
-  router.post("/", role.create);
+  router.post("/", checkAuth, role.create);
   //find all customers
-  router.get("/", role.findAll);
+  router.get("/", checkAuth,role.findAll);
   // //find customer by id
-  router.get("/:id", role.findOne);
+  router.get("/:id", checkAuth, role.findOne);
   // //update a customer
-  router.put("/:id",role.update);
+  router.put("/:id", checkAuth,role.update);
   // //delete customer
-  router.delete("/:id",role.delete);
+  router.delete("/:id", checkAuth,role.delete);
 
   app.use("/api/role", router);
 
