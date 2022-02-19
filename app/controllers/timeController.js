47316fb1bcd3256f6b44f6404d7a14ab6ab02abf
cerenunlu <cerennlu01@outlook.com>
2022-02-19 10:51:07 +0300

    find all fixed and select hours function added

--- app/controllers/timeController.js
+++ app/controllers/timeController.js
@@ -4,6 +4,20 @@ const Op=db.Sequelize.Op;
 var _ = require("underscore");
 
 
+
+exports.findAll=(req,res)=>{
+    let employeeid = req.params.id;
+    console.log(employeeid);
+    db.Time.findAll({
+        where: {
+            employee_id:employeeid,
+            available:1
+        }
+    }).then(function (available_times) {
+        res.json(available_times);
+    })
+}
+
 exports.create=(req,res)=>{
     let body = _.pick(req.body, 
         "hour",
@@ -87,3 +101,54 @@ exports.update = (req, res) => {
     })
 
 }
+
+exports.selectHours = (req, res) => {
+    let time_id = req.params.id;
+
+    
+    db.Time.findOne({
+        where: {
+            id:time_id
+        }
+    }).then(function (selectedTime) {
+
+        let body = _.pick(selectedTime,
+            "hour",
+            "available",
+            "date_id",
+            "employee_id"
+            );
+        let attributes = {};
+
+        body.hour=selectedTime.hour,
+        body.available=0,
+        body.date_id=selectedTime.date_id,
+        body.employee_id=selectedTime.employee_id
+        if (body.hasOwnProperty("hour")) {
+            attributes.hour = body.hour;
+        }
+        if (body.hasOwnProperty("available")) {
+            attributes.available = body.available;
+        }
+        if (body.hasOwnProperty("date_id")) {
+            attributes.date_id = body.date_id;
+        }
+        if (body.hasOwnProperty("employee_id")) {
+            attributes.employee_id = body.employee_id;
+        }
+        console.log(attributes.available);
+        if (selectedTime) {
+            selectedTime.update(attributes).then(function (selectedTime) {
+                res.json(selectedTime.toJSON());
+            }, function () {
+                res.status(400).send();
+            })
+        } else {
+            res.status(404).send({
+                error: "id not found!"
+            })
+        }
+    })
+
+}
+
