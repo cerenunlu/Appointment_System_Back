c870570cdd6360040d73a13ffce4b92f64f87ee0
cerenunlu <cerennlu01@outlook.com>
2022-03-18 18:16:57 +0300

    refactor

---
+++ app/database/db.js
@@ -0,0 +1,8 @@
+
+const Customer = require('../models/customer');
+const CustomerEmployee = require('../models/customer_employee');
+const Department = require('../models/department');
+const Appointment = require('../models/appointment');
+const Employee = require('../models/employee');
+const index = require('../models/index');
+const Role = require('../models/role');
